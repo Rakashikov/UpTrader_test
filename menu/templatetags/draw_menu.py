703226070b712cf56294e2b49740c91f61b38559
Evgeniy Rakashikov <evgeniyhd@gmail.com>
2023-02-23 12:50:09 +0600

    Created menu

--- menu/templatetags/draw_menu.py
+++ menu/templatetags/draw_menu.py
@@ -1,5 +1,4 @@
 from django import template
-from django.db.models import Q
 
 from ..models import MenuItem
 
@@ -8,6 +7,18 @@ register = template.Library()
 
 @register.inclusion_tag('template_tag/menu.html', takes_context=True)
 def draw_menu(context, slug):
-    menu_items = MenuItem.objects.filter(Q(menu__slug=slug) &
-                                         (Q(parent=None) | Q(parent__slug=context['slug'])))
-    return {'menu': menu_items.get(slug=context['slug']).menu.title, 'menu_items': menu_items}
+    menu_items = MenuItem.objects.filter(menu__slug=slug)
+    menu_title = menu_items.all()[:1].get().menu.title
+    item = None
+    try:
+        if context['slug'] != "favicon.ico":
+            item = menu_items.get(slug=context['slug'])
+            if item.parent is not None:
+                item = item.parent
+            items = [x for x in menu_items if x.parent is None]
+            items = items[items.index(item) + 1:]
+            for i in items:
+                menu_items = menu_items.exclude(parent__slug=i.slug)
+            return {'menu': menu_title, 'menu_items': menu_items}
+    except:
+        return {'menu': menu_title, 'menu_items': menu_items.filter(parent=None)}
